de45e93d78d90aab2bba382469a3118223aefd1b
Mukund <mukundchourey@gmail.com>
2021-04-21 15:50:46 +0530

    login route added

--- app/routes/vendor.routes.js
+++ app/routes/vendor.routes.js
@@ -11,7 +11,7 @@ module.exports = (app) => {
     app.post('/register', vendorReg.register);
 
     //vendor login  
-    // app.post('/vendorlogin', vendorReg.login);
+    app.post('/vendorlogin', vendorReg.login);
 
     
 
